9535132b5b897bf37845c7f8a2bbfbf9bd27a352
Connor Duignan <confkins@gmail.com>
2019-02-28 15:53:44 +0000

    Updated Control Code

---
+++ Damage Control/Assets/Selector.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selector : MonoBehaviour {
+
+    public bool isSelected = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                if (hit.collider.tag == "Pawn")
+                {
+                    isSelected = true;
+                }
+                else
+                {
+                    isSelected = false;
+                }
+            }
+        }
+    }
+
+}
