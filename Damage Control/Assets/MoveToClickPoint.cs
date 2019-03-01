8894d848370b1f912bcb41793c5050e6f86d9e8a
Connor Duignan <confkins@gmail.com>
2019-03-01 13:30:34 +0000

    INtegrated Selector and Movement

--- Damage Control/Assets/MoveToClickPoint.cs
+++ Damage Control/Assets/MoveToClickPoint.cs
@@ -5,16 +5,42 @@ using UnityEngine.AI;
 
 public class MoveToClickPoint : MonoBehaviour
 {
+
+    public bool isSelected;
     NavMeshAgent agent;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        
     }
+
+    IEnumerator Selector()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                if (hit.collider.tag == "Pawn")
+                {
+                    isSelected = true;
+                }
+                else
+                {
+                    isSelected = false;
+                }
+            }
+        }
+        yield return null;
+    }
+
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        StartCoroutine("Selector");
+        if (Input.GetMouseButtonDown(1) && isSelected)
         {
             RaycastHit hit;
 
