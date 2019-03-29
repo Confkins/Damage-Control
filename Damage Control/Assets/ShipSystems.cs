8879e3d4114191a9db8991b4f1077104cdcc24d1
Connor Duignan <confkins@gmail.com>
2019-03-29 13:28:53 +0000

    Fixed Collision Detection

--- Damage Control/Assets/ShipSystems.cs
+++ Damage Control/Assets/ShipSystems.cs
@@ -5,12 +5,26 @@ using UnityEngine;
 public class ShipSystems : SystemSuper
 {
     public ShipSystems()
-    { 
+    {
+        hitPointCounter = 10;
 
+    }
+
+    void Start()
+    {
+        this.subSystem = gameObject;
+        Rigidbody rigidBody = gameObject.AddComponent<Rigidbody>();
+        rigidBody.useGravity = false;
+        rigidBody.isKinematic = false;
+        rigidBody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        operationalRange = subSystem.GetComponent<BoxCollider>();
+        operationalRange.size = new Vector3(radius,1,radius);
+        operationalRange.isTrigger = true;
     }
 
     public void OnTriggerEnter(Collider other)
     {
+        Debug.Log(other);
         Dismantle();
     }
 }
--- Damage Control/Assets/SystemSuper.cs
+++ Damage Control/Assets/SystemSuper.cs
@@ -7,13 +7,14 @@ public class SystemSuper : MonoBehaviour
     public int hitPointCounter;
     public int maxHitPoints;
     public GameObject subSystem;
-    public int radius = 2;
+    public int radius = 3;
     public int sparePartCounter = 0;
+    public BoxCollider operationalRange;
 
     public void Dismantle()
     {
-        //Debug.Log(hitPointCounter);
-        //Debug.Log(sparePartCounter);
+        Debug.Log(hitPointCounter);
+        Debug.Log(sparePartCounter);
 
         while (hitPointCounter != 0)
         {
@@ -23,6 +24,7 @@ public class SystemSuper : MonoBehaviour
     }
 
     public void Repair()
-    {   
+    {
+           
     }
 }
