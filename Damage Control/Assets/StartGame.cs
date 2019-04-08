43366d1d22cb8f204971681fe8c74c3e9076a21b
Connor Duignan <confkins@gmail.com>
2019-04-08 13:37:40 +0100

    Added Start Screen, created first executable

---
+++ Damage Control/Assets/StartGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartGame : MonoBehaviour
+{
+    public Button mybutton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mybutton.onClick.AddListener(loadGame);
+    }
+
+    void loadGame()
+    {
+        SceneManager.LoadScene("MainGame", LoadSceneMode.Single);
+    }
+}
